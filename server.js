79c073be247ec59aad723b201a8d34c01bfea03d
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2021-07-09 23:56:45 +0530

    db connection

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+const mongoose = require('mongoose');
+const _CONST = require('./config/constant');
 const app = require('./app');
+
+mongoose.connect(_CONST.DB_URI, _CONST.DB_OPTIONS, (err) => {
+  if (err) {
+    console.error('DB Connection Error...');
+    process.exit(1);
+  }
+  console.log('DB Connected...');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App is running on ${PORT}`));
