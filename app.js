17d634cc87356a4d3c0a6b554c39e733cbb90561
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2021-07-28 16:58:31 +0530

    disabled swagger for time being

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const swagger = require('swagger-ui-express');
 
 const tourRouter = require('./routes/TourRoutes');
 const userRouter = require('./routes/UserRoutes');
-const swaggerDoc = require('./swagger/swagger.json');
+// const swaggerDoc = require('./swagger/swagger.json');
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`));
 
-app.use('/api/docs/v1', swagger.serve, swagger.setup(swaggerDoc));
+// app.use('/api/docs/v1', swagger.serve, swagger.setup(swaggerDoc));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
