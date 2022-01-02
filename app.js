52926cd1986976ac670b82233c1aceac2c0550d0
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2022-01-02 17:37:31 +0530

    swagger

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const swagger = require('swagger-ui-express');
 const { health } = require('./controllers/Health');
 const tourRouter = require('./routes/TourRoutes');
 const userRouter = require('./routes/UserRoutes');
-// const swaggerDoc = require('./swagger/swagger.json');
+const swaggerDoc = require('./swagger/swagger.json');
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`));
 
-// app.use('/api/docs/v1', swagger.serve, swagger.setup(swaggerDoc));
+app.use('/api/v1/docs', swagger.serve, swagger.setup(swaggerDoc));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
