49d62e43d1074146b77ecb118fd8ee30459793c9
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2022-01-02 17:40:45 +0530

    user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -35,4 +35,36 @@ const deleteUser = (req, res) => {
   });
 };
 
+/* const login = async (request, response) => {
+  if (!request?.body?.username || !request?.body?.password) {
+    logger.warn('Missing username or password:', logger.pretty(request.body));
+    return response.status(RETURN_CODES.PRECONDITION_FAILED).send({ error: 'Missing username or password' });
+  }
+  try {
+    let loginResponse = await usersHandler.loginUser(request.body);
+    if (loginResponse.error) {
+      return response.status(RETURN_CODES.UNATHORIZED_STATUS).send({ message: 'Not authorized' });
+    }
+    logger.info(request?.body?.username, '=> user logged in');
+    let filteredloginResponse = _.pick(loginResponse, ['name', 'userName', 'surname', 'email', 'role']); //only need these fields
+    let token = tokenHandler.sign(filteredloginResponse);
+    tokenHandler.addTokenToResponse(response, token); //add to cookies
+    return response.status(RETURN_CODES.OK_STATUS).send({ user: loginResponse, token: token });
+  } catch (error) {
+    logger.error(error);
+    return response.status(RETURN_CODES.INTERNAL_ERROR_STATUS).send({ error: 'Internal error' });
+  }
+};
+
+const logout = async (request, response) => {
+  try {
+    logger.info('Logout request');
+    tokenHandler.deleteToken(response); //delete from cookies, too
+    return response.status(RETURN_CODES.OK_STATUS).send({ message: 'User logged out' });
+  } catch (error) {
+    logger.error(error);
+    return response.status(RETURN_CODES.INTERNAL_ERROR_STATUS).send({ error: 'Internal error' });
+  }
+}; */
+
 module.exports = { getAllUsers, getUser, updateUser, createUser, deleteUser };
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -13,4 +13,26 @@ router
   .patch(UserController.updateUser)
   .delete(UserController.deleteUser);
 
+/* router.post('/login', UserController.login);
+router.post('/logout', UserController.logout);
+router.delete('/:userName', async (request, response) => {
+  let sessionCheckResult = tokenHandler.checkSessionValidity(request, response);
+  if (sessionCheckResult.error || !sessionCheckResult?.admin) {
+    return response.status(RETURN_CODES.UNATHORIZED_STATUS).send({ message: 'Not authorized' });
+  }
+  if (!request?.params?.userName) {
+    return response.status(RETURN_CODES.PRECONDITION_FAILED).send({ message: 'Missing user name parameter' });
+  }
+  try {
+    let deleteResponse = await usersHandler.deleteUser(request?.params?.userName);
+    if (!deleteResponse.error) {
+      return response.status(RETURN_CODES.OK_STATUS).send({ message: 'User deleted' });
+    } else {
+      return response.status(RETURN_CODES.INTERNAL_ERROR_STATUS).send({ error: deleteResponse.error });
+    }
+  } catch (error) {
+    logger.error(error);
+    return response.status(RETURN_CODES.INTERNAL_ERROR_STATUS).send({ error: 'Internal error' });
+  }
+}); */
 module.exports = router;
